d7a09735a5df2d03ad37eef1f30317ef63683981
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-01-14 00:20:29 +0600

    Advanced Search Using OR & HAS Conditions

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,29 +1,46 @@
 import { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const createPost = async (data:Omit<Post, "id"|"createdAt"|"updatedAt"|"authorId">, userId:string)=>{
+const createPost = async (data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId: string) => {
     const result = await prisma.post.create({
-        data:{
+        data: {
             ...data,
-            authorId:userId
+            authorId: userId
         }
     })
     return result
 }
 
-const getAllPost=async(payload:{search: string | undefined})=>{
+const getAllPost = async (payload: { search: string | undefined }) => {
     const allPosts = await prisma.post.findMany({
-        where:{
-            title:{
-                contains:payload.search as string,
-                mode:"insensitive"
-            }
+        where: {
+            OR: [
+                {
+                    title: {
+                        contains: payload.search as string,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    content: {
+                        contains: payload.search as string,
+                        mode: "insensitive"
+                    }
+                },
+                {
+                    tags: {
+                        has:payload.search as string,
+                        
+                    }
+                }
+            ]
+
         }
     })
     return allPosts
 }
 
-export const postService={
- createPost,
- getAllPost
+export const postService = {
+    createPost,
+    getAllPost
 }
